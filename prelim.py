3f95315e8f38dabba32caefde548d93d5c0b4dc2
stlgolfer <alexander.migala@duke.edu>
2022-06-01 20:39:51 -0400

    Can now simulate data with snowglobes

--- prelim.py
+++ prelim.py
@@ -9,6 +9,7 @@ from astropy import units as u
 from snewpy.neutrino import Flavor, MassHierarchy
 from snewpy.models import Nakazato_2013
 from snewpy.flavor_transformation import NoTransformation # just use NoTransformation for now to keep things simple
+import os
 
 # snewpy-snowglobes stuff
 import snewpy.snowglobes as snowglobes
@@ -17,6 +18,10 @@ modelFilePathBase = "./SNEWPY_models/Nakazato_2013/"
 modelFilePath = modelFilePathBase + "nakazato-shen-z0.004-t_rev100ms-s20.0.fits"
 model = Nakazato_2013(modelFilePath)
 snowglobes_out_name = "snowglobes-output"
+snowglobes_dir = os.environ['SNOWGLOBES']
+tball_suffix = '.tar.bz2'
+#print(os.environ['SNOWGLOBES'])
+smearing = False
 model
 
 # we have the model loaded, so let's try to generate the fluence files using SNEWPY
@@ -25,5 +30,13 @@ snowglobes.generate_fluence(model_path=modelFilePath,model_type="Nakazato_2013",
                             d=10,
                             output_filename=snowglobes_out_name
                             )
-snowglobes.collate(tarball_path=modelFilePathBase+snowglobes_out_name,
-                   smearing=False,skip_plots=False,SNOwGLoBESdir='$SNOWGLOBES')
+
+# also need to run the simulation so that we get the correct output
+snowglobes.simulate(SNOwGLoBESdir=snowglobes_dir,
+                    tarball_path=modelFilePathBase+snowglobes_out_name+tball_suffix,
+                    detector_effects=smearing,
+                    detector_input="all"
+                    )
+
+snowglobes.collate(tarball_path=modelFilePathBase+snowglobes_out_name+tball_suffix,
+                   smearing=False,skip_plots=False,SNOwGLoBESdir=snowglobes_dir)
