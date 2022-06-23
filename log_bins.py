956ceee0291e520512f037a5b2c49c7a7838d13b
stlgolfer <alexander.migala@duke.edu>
2022-06-22 20:16:41 -0400

    Testing log bins

--- log_bins.py
+++ log_bins.py
@@ -39,21 +39,6 @@ transform = "AdiabaticMSW_NMO"
 print(f'Timeframe of model is from {model.time[0]} to {model.time[len(model.time)-1]}')
 
 profiles = handlers.build_detector_profiles()
-# create scintillator detector analysis
-plot_data, raw_data, l_data = t.create_detector_event_scatter(
-    modelFilePath,model_type,
-    detector,
-    model,
-    deltat=deltat,
-    transformation=transform,
-    data_calc=profiles[detector]['handler'],
-    use_cache=True
-    )
-t.create_default_detector_plot(plot_data, profiles[detector]['axes'](), f'Nakazato {transform} dt={str(deltat)}')
-
-t.create_regular_plot(raw_data, profiles[detector]['axes'](),
-                      '{model} {detector} {transform}'.format(model=model_type,detector=detector,transform=transform),
-                      ylab="Event Counts",use_x_log=False,save=True)
 
 for detector in profiles.keys():
     l_plot_data, l_raw_data, l_l_data = t.create_detector_event_scatter(
@@ -69,6 +54,17 @@ for detector in profiles.keys():
     
     t.create_regular_plot(l_raw_data, ['ibd','nue+es','nc'],
                           '{model} {detector} {transform} Logged Bins'.format(model=model_type,detector=detector,transform=transform),
-                          ylab="Event Counts",xlab="Logged Time Bin No",use_x_log=False,save=True,show=False)
+                          ylab="Event Counts",xlab="Logged Time Bin No",use_x_log=False,save=True,show=True)
     t.create_default_detector_plot(l_plot_data, profiles[detector]['axes'](),
-                                   f'Nakazato {detector} {transform} dt={str(deltat)} Logged Bins',show=False)
+                                   f'Nakazato {detector} {transform} dt={str(deltat)} Logged Bins',show=True)
+    # also create regular plot for comparison
+    plot_data, raw_data, l_data = t.create_detector_event_scatter(
+        modelFilePath,model_type,
+        detector,
+        model,
+        deltat=deltat,
+        transformation=transform,
+        data_calc=profiles[detector]['handler'],
+        use_cache=True
+        )
+    t.create_default_detector_plot(plot_data, profiles[detector]['axes'](), f'Nakazato {detector} {transform} dt={str(deltat)}',save=True)
