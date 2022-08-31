06af51f2bb66b39bbfee53afebd1a56b19941715
Alex Migala <acm123@phy-compute-03.phy.duke.edu>
2022-08-30 23:58:29 -0400

    Remove bad imports

--- snowglobes_wrapper.py
+++ snowglobes_wrapper.py
@@ -40,7 +40,7 @@ import math
 import snewpy.models
 from snewpy.flavor_transformation import *
 from snewpy.neutrino import Flavor, MassHierarchy
-from snewpy.snowglobes_interface import SNOwGLoBES, SimpleRate
+#from snewpy.snowglobes_interface import SNOwGLoBES, SimpleRate
 
 logger = logging.getLogger(__name__)
 
