a3b543b4bb8fa3fc61f23da09f53af88659024bd
stlgolfer <alexander.migala@duke.edu>
2022-09-16 18:57:46 -0400

    Store class instead of instance for snewpy models

--- model_wrappers.py
+++ model_wrappers.py
@@ -65,7 +65,7 @@ snewpy_models['Zha_2021'] = SNEWPYModel(
         './SNEWPY_models/Zha_2021/s33.dat'
     ],
     'Zha_2021',
-    Zha_2021(fname)
+    Zha_2021
     )
 
 '''
@@ -81,7 +81,7 @@ snewpy_models['Warren_2020'] = SNEWPYModel(
         './SNEWPY_models/Warren_2020/stir_a1.25/stir_multimessenger_a1.25_m55.0.h5'
     ],
     'Warren_2020',
-    Warren_2020(fname)
+    Warren_2020
     )
 
 '''
@@ -91,7 +91,7 @@ fname = './SNEWPY_models/Walk_2018/s15.0c_3D_nonrot_dir1'
 snewpy_models['Walk_2018'] = SNEWPYModel(
     [fname],
     'Walk_2018',
-    Walk_2018(fname)
+    Walk_2018
     )
 
 '''
@@ -104,7 +104,7 @@ snewpy_models['Tamborra_2014'] = SNEWPYModel(
         './SNEWPY_models/Tamborra_2014/s27.0c_3D_dir1'
     ],
     'Tamborra_2014',
-    Tamborra_2014(fname, 'LS220')
+    Tamborra_2014
     )
 
 '''
@@ -119,7 +119,7 @@ snewpy_models['Sukhbold_2015'] = SNEWPYModel(
         './SNEWPY_models/Sukhbold_2015/sukhbold-LS220-z9.6.fits'
     ],
     'Sukhbold_2015',
-    Sukhbold_2015(fname)
+    Sukhbold_2015
     )
 
 '''
@@ -134,7 +134,7 @@ snewpy_models['Kuroda_2020'] = SNEWPYModel(
         './SNEWPY_models/Kuroda_2020/LnuR10B13.dat'
     ],
     'Kuroda_2020',
-    Kuroda_2020(fname)
+    Kuroda_2020
     )
 
 '''
@@ -159,7 +159,7 @@ snewpy_models['Fornax_2021'] = SNEWPYModel(
         './SNEWPY_models/Fornax_2021/lum_spec_26M_r10000_dat.h5'
     ],
     'Fornax_2021',
-    Fornax_2021(fname)
+    Fornax_2021
     )
 
 '''
@@ -195,7 +195,7 @@ snewpy_models['Nakazato_2013'] = SNEWPYModel(
         './SNEWPY_models/Nakazato_2013/nakazato-shen-z0.02-t_rev300ms-s13.0.fits'
     ],
     'Nakazato_2013',
-    Nakazato_2013(fname)
+    Nakazato_2013
     )
 
 '''
@@ -208,7 +208,7 @@ snewpy_models['Bollig_2016'] = SNEWPYModel(
         './SNEWPY_models/Bollig_2016/s27.0c'
     ],
     'Bollig_2016',
-    Bollig_2016(fname)
+    Bollig_2016
     )
 
 '''
@@ -218,7 +218,7 @@ fname = './SNEWPY_models/Walk_2019/s40.0c_3DBH_dir1'
 snewpy_models['Walk_2019'] = SNEWPYModel(
     [fname],
     'Walk_2019',
-    Bollig_2016(fname)
+    Bollig_2016
     )
 
 
