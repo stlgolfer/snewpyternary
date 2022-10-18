a0f51188e96f34dfdec2257f77bce86fcf75df3e
stlgolfer <alexander.migala@duke.edu>
2022-10-18 12:51:40 -0400

    Detector config abstraction, but now a lot of code will break

--- data_handlers.py
+++ data_handlers.py
@@ -7,7 +7,7 @@ This contains data handlers for handling individual model data
 """
 
 import numpy as np
-import os
+from abc import ABC, abstractmethod
 
 supported_detectors = [
     'scint20kt',
@@ -15,65 +15,82 @@ supported_detectors = [
     'wc100kt30prct'
     ]
 
-def build_detector_profiles():
+class DetectorProxyConfiguration(ABC):
     '''
-    Builds a profile for a detector that includes a dictionary of data handlers
-    and the corresponding data labels (used for labeling axes)
+    What follows are the calculations that are made for each time bin when collated
+    data comes through. Each time bin has event rates per energy bin. Order should
+    be nc, nu_e+es, then inverse beta decay events.
 
-    Returns
-    -------
-    detector_defs : dictionary
-        The dictionary of profiles
+    Treat all the NC as nux,  all the nue-nucleus and ES as nue, and IBD+nuebar-nucleus as nuebar.
+
+    Each function here should return nux, nue, and anue, respectively if using the same_axes label
+    '''
+
+    @abstractmethod
+    def h_scint20kt(self):
+        pass
+
+    @abstractmethod
+    def h_ar40kt(self):
+        pass
+
+    @abstractmethod
+    def h_wc100kt30prct(self):
+        pass
 
     '''
-    detector_defs = {}
-    module_scope = __import__(__name__)
-    for detector in supported_detectors:
-        detector_defs[detector] = {
-            'handler':getattr(module_scope, f'h_{detector}'),
-            'axes':getattr(module_scope,f'axes_{detector}')
-            }
-    return detector_defs
+    Data handler axes labels definitions
+    '''
 
-'''
-What follows are the calculations that are made for each time bin when collated
-data comes through. Each time bin has event rates per energy bin. Order should
-be nc, nu_e+es, then inverse beta decay events.
+    def same_axes(self):
+        return [r'$\nu_x$ Proxy', r'$\nu_e$ Proxy', r'$\bar{\nu_e}$ Proxy']
 
-Treat all the NC as nux,  all the nue-nucleus and ES as nue, and IBD+nuebar-nucleus as nuebar
-'''
+    def axes_scint20kt(self):
+        return self.same_axes()
 
-def h_scint20kt(data):
-    # must return a list of a, b, c
-    nue_plus_es=np.sum(data['nue_C12'])+np.sum(data['nue_C13']+data['e']) # nue
-    ibd = np.sum(data['ibd'])
-    nc = np.sum(data['nc'])
-    return [nc,nue_plus_es,ibd]
+    def axes_ar40kt(self):
+        return self.same_axes()
 
-def h_ar40kt(data):
-    nue_plus_es = np.sum(data['nue_Ar40'])+np.sum(data['e'])
-    nc = np.sum(data['nc'])
-    nue_bar = np.sum(data['nuebar_Ar40'])
-    return [nc,nue_plus_es,nue_bar]
+    def axes_wc100kt30prct(self):
+        return self.same_axes()
 
-def h_wc100kt30prct(data):
-    ibd = np.sum(data['ibd'])
-    nue_plus_es=np.sum(data['nue_O16'])+np.sum(data['e'])
-    nc = np.sum(data['nc'])
-    return [nc,nue_plus_es,ibd]
+class ConfigAggregateDetectors(DetectorProxyConfiguration):
 
-'''
-Data handler axes labels definitions
-'''
+    def h_scint20kt(data):
+        # must return a list of a, b, c
+        nue_plus_es=np.sum(data['nue_C12'])+np.sum(data['nue_C13']+data['e']) # nue
+        ibd = np.sum(data['ibd'])
+        nc = np.sum(data['nc'])
+        return [nc,nue_plus_es,ibd]
 
-def same_axes():
-    return [r'$\nu_x$ Proxy',r'$\nu_e$ Proxy',r'$\bar{\nu_e}$ Proxy']
+    def h_ar40kt(data):
+        nue_plus_es = np.sum(data['nue_Ar40'])+np.sum(data['e'])
+        nc = np.sum(data['nc'])
+        nue_bar = np.sum(data['nuebar_Ar40'])
+        return [nc,nue_plus_es,nue_bar]
 
-def axes_scint20kt():
-    return same_axes()
+    def h_wc100kt30prct(data):
+        ibd = np.sum(data['ibd'])
+        nue_plus_es=np.sum(data['nue_O16'])+np.sum(data['e'])
+        nc = np.sum(data['nc'])
+        return [nc,nue_plus_es,ibd]
 
-def axes_ar40kt():
-    return same_axes()
-
-def axes_wc100kt30prct():
-    return same_axes()
+# def build_detector_profiles():
+#     '''
+#     Builds a profile for a detector that includes a dictionary of data handlers
+#     and the corresponding data labels (used for labeling axes)
+#
+#     Returns
+#     -------
+#     detector_defs : dictionary
+#         The dictionary of profiles
+#
+#     '''
+#     detector_defs = {}
+#     module_scope = __import__(__name__)
+#     for detector in supported_detectors:
+#         detector_defs[detector] = {
+#             'handler':getattr(module_scope, f'h_{detector}'),
+#             'axes':getattr(module_scope,f'axes_{detector}')
+#             }
+#     return detector_defs
