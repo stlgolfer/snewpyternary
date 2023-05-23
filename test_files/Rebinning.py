8a15d2c8d3a0d8d8a300c89f12e4bfe4e5385f6c
stlgolfer <alexander.migala@duke.edu>
2023-05-23 14:19:44 -0400

    Edge cases

--- test_files/Rebinning.py
+++ test_files/Rebinning.py
@@ -4,7 +4,7 @@ from scipy.stats import gaussian_kde
 
 # so we have an array that gets turned into a histogram
 arr = [3,3,3,1,5]
-
+wider_arr = [-1,0,3,3,7]
 hist, bin_edges = np.histogram(np.array(arr), bins=5)
 
 # for now, just make the binning an interval of the other binning for a sanity check
@@ -14,28 +14,28 @@ hist_fine, bin_edges_fine = np.histogram(np.array(arr), bins=10)
 # nah, this shit way too hard
 
 plt.figure()
-plt.plot(bin_edges[:-1], hist, label='Regular')
-plt.plot(bin_edges_fine[:-1], hist_fine, label='Fine')
+plt.bar(bin_edges[:-1], hist, label='Regular')
+plt.bar(bin_edges_fine[:-1], hist_fine, label='Fine')
 # print(hist.copy().resize(hist_fine.copy().shape))
 # plt.plot(bin_edges[:-1], np.resize(hist_fine, hist.shape), label='Hist Fine Reshaped')
 
 
 # ok so start with bigger bin and go piece by piece
+# TODO: now we just need the range to be the same, which will just be a pre-processing thing
+# TODO: also need the case when the large bin is smaller than the fine bin
+# we want to map to the fine one, so cut off values
 mult_bin_edges = bin_edges_fine # also for now assume they have the same range, but we would take the smaller one
 def find_num_in_range(edges, graph, num) -> float:
-    index = -1
     for i, e in enumerate(edges):
         if num >= e:
-            index = i
-    return graph[index]
+            return graph[i-1]
+    return 0
 mult = np.zeros_like(hist_fine)
-for bin_index, small_bins in enumerate(bin_edges_fine):
-    if bin_index < len(bin_edges_fine) - 1:
+for bin_index, small_bin in enumerate(bin_edges_fine):
+    if bin_index < len(bin_edges_fine) - 1 and small_bin > min(bin_edges) and small_bin < max(bin_edges):
         val = hist_fine[bin_index]
         mult[bin_index] = find_num_in_range(bin_edges, hist, val) * val
-plt.plot(mult_bin_edges[:-1], mult, label="Multiply")
+plt.plot(mult_bin_edges[:-1], mult, '--', label="Multiply")
 plt.legend()
 plt.show()
-
-#TODO: now we just need the range to be the same, which will just be a pre-processing thing
 print("yuh")
