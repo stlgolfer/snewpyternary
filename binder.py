c800c49b13eac1855fcc7ad7c3644d6858efced0
stlgolfer <alexander.migala@duke.edu>
2023-07-21 15:22:51 -0400

    Add title option

--- binder.py
+++ binder.py
@@ -13,7 +13,8 @@ import matplotlib.pyplot as plt
 @click.option('-nux', required=True, help='Location of nux csv')
 @click.option('-nue', required=True, help='Location of nue csv')
 @click.option('-anue', required=True, help='Location of anue csv')
-def bind(nux, nue, anue):
+@click.option('--title', required=False, help='Title of the ternary plot', default='')
+def bind(nux, nue, anue, title):
     nux_df = pd.read_csv(nux)
     nue_df = pd.read_csv(nue)
     anue_df = pd.read_csv(anue)
@@ -21,10 +22,10 @@ def bind(nux, nue, anue):
     # now that everything is loaded, need to put df into raw tuples
     raw_combined = list(zip(nux_df['unfolded'], anue_df['unfolded'], nue_df['unfolded']))
     ternary_points = t_normalize(raw_combined)
-    print(ternary_points)
+    # print(ternary_points)
     # get the heatmap of it as well
 
-    fig, tax = t.create_default_flux_plot(ternary_points, "yuh", save=False, show=False)
+    fig, tax = t.create_default_flux_plot(ternary_points, title, save=False, show=False)
     print("Generating heatmap (this might take a while)...")
     tax.heatmap(generate_heatmap_dict(raw_combined, ternary_points), cmap=plt.get_cmap('PiYG'))
     print("Done")
