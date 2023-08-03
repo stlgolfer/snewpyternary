8dcd8fa9c8ab99f8676d0e664ec3c8c7d7045f6f
stlgolfer <alexander.migala@duke.edu>
2023-08-03 15:35:51 -0400

    Using LA approach for phi_est errors

--- binder.py
+++ binder.py
@@ -44,12 +44,12 @@ def bind(nux, nue, anue, title):
 
     fig, tax = t.create_default_flux_plot(ternary_points, title, save=False, show=False)
     print("Generating heatmap (this might take a while)...")
-    # tax.heatmap(generate_heatmap_dict_phi_est(raw_combined, ternary_points, ndet_raw_combined), cmap=plt.get_cmap('PiYG'))
+    tax.heatmap(generate_heatmap_dict_phi_est(raw_combined, ternary_points, ndet_raw_combined, nux_df['dt']), cmap=plt.get_cmap('PiYG'))
     print("Done")
     tax.show()
 
     Ndet_fig, Ndet_tax = t.create_default_flux_plot(t_normalize(ndet_raw_combined), "",save=False,show=False)
-    Ndet_tax.heatmap(generate_heatmap_dict(ndet_raw_combined,t_normalize(ndet_raw_combined),nux_df['dt']))
+    # Ndet_tax.heatmap(generate_heatmap_dict(ndet_raw_combined,t_normalize(ndet_raw_combined),nux_df['dt']))
     Ndet_tax.show()
 
 if __name__ == '__main__':
