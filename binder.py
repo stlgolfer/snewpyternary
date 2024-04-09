Fix Ndet calculation in sigma_average

binder.py
@@ -157,7 +157,7 @@ def bind(nux, nue, anue, title, heatmap):
     td_c_ax.errorbar(nux_df['time'], np.cumsum(nue_df['unfolded']), yerr=nue_csum_error_td, fmt='.', label=r'$\nu_e$ Unf.',errorevery=5)
     td_c_ax.set_xscale('log')
     td_c_ax.set_yscale('log')
-    td_c_ax.set_xlabel('Mid-Point Time (s)')
+    td_c_ax.set_xlabel('Mid-Point Time (s) + t0')
     td_c_ax.set_ylabel(r'$\frac{neutrinos}{0.2*MeV*dt}$ Cumu.')
     td_c_ax.legend()
 
@@ -201,7 +201,7 @@ def bind(nux, nue, anue, title, heatmap):
         errorevery=5
     )
     td_c_frac_ax.set_xscale('log')
-    td_c_frac_ax.set_xlabel('Mid-Point Time (s)')
+    td_c_frac_ax.set_xlabel('Mid-Point Time (s) + t0')
     td_c_frac_ax.set_ylabel('%')
     td_c_frac_ax.set_ylim(0, 100)
     td_c_frac_ax.legend()
@@ -244,7 +244,7 @@ def bind(nux, nue, anue, title, heatmap):
         errorevery=5
     )
     td_frac_ax.set_xscale('log')
-    td_frac_ax.set_xlabel('Mid-Point Time (s)')
+    td_frac_ax.set_xlabel('Mid-Point Time (s) + t0')
     td_frac_ax.set_ylim(0,100)
     td_frac_ax.set_ylabel('%')
     td_frac_ax.legend()
@@ -257,7 +257,7 @@ def bind(nux, nue, anue, title, heatmap):
         errorevery=5)
     td_ax.set_xscale('log')
     td_ax.set_yscale('log')
-    td_ax.set_xlabel('Mid-Point Time (s)')
+    td_ax.set_xlabel('Mid-Point Time (s) + t0')
     td_ax.set_ylabel(r'$\frac{neutrinos}{0.2*MeV*dt}$')
     td_ax.legend()
     # let's also make the same plot but in regular y-scale

meta_analysis.py
@@ -163,7 +163,7 @@ def process_detector(config: t.MetaAnalysisConfig, set_no: int, detector: str) -
                 spt_full_content = np.column_stack((spt_full_content, spt_content[sptc_index[detector]['index']]))
             pbar.update(1)
     spt_ax.set_ylabel('Energy (GeV)')
-    spt_ax.set_xlabel('Time (s)')
+    spt_ax.set_xlabel(r'Time + $t_0$ (s)')
     # spt_ax.set_zlabel('Event rate')
     spt_ax.set_title(spt_title)
     # x dim should be energy bins, y should be time?

sigma_average.py
@@ -64,7 +64,7 @@ def estimate_cxn(
 
     phi_t_ax.scatter(times_unitless, phi_t_over_time)
     phi_t_ax.set_title(rf'$\phi_t$ for {config.model_type} {config.set_numbers[0]} Fluence')
-    phi_t_ax.set_xlabel("Time (s)")
+    phi_t_ax.set_xlabel(r"Time + $t_0$ (s)")
     phi_t_ax.set_ylabel(r'$neutrinos/cm^2$')
     phi_t_ax.set_xscale('log')
     phi_t_inset_ax = phi_t_ax.inset_axes([0.2,0.2,0.5,0.5])
@@ -211,7 +211,7 @@ def estimate_cxn(
         Ndet = dts[phi_est_time_bin] * 0.2e-3 * np.sum(
             l_data[phi_est_time_bin][det_chan_name]
         )
-        Ndet_over_time[phi_est_time_bin] = Ndet #/(0.2e-3 * dts[phi_est_time_bin])
+        Ndet_over_time[phi_est_time_bin] = Ndet /(0.2e-3 * dts[phi_est_time_bin])
 
         phi_est_unfolded[phi_est_time_bin] = Ndet / (Nt * sigma_average[phi_est_time_bin])
     unfold_fig, unfold_ax = plt.subplots(1,1)
@@ -221,6 +221,16 @@ def estimate_cxn(
     unfold_ax.set_title(f'{config.stringify(submodel_number)} {cxn_truth_chan_key} Unfolded')
     unfold_ax.legend()
     unfold_fig.show()
+    #endregion
+
+    #region also plot the Ndet_over_time as a general check
+    ndet_fig, ndet_ax = plt.subplots(1)
+    ndet_ax.set_xscale('log')
+    ndet_ax.scatter(times_unitless, Ndet_over_time)
+    ndet_ax.set_title(f'{config.stringify(submodel_number)} {cxn_truth_chan_key} Detector Count')
+    ndet_ax.set_xlabel('Time + t0 (s)')
+    ndet_ax.set_ylabel('Detector Count')
+    ndet_fig.show()
     #endregion
 
     if __name__ == '__main__':