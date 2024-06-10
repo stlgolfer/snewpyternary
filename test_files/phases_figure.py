a1fbe3c534be519f54feadac21e152b0fa0810ab
stlgolfer <alexander.migala@duke.edu>
2024-06-10 19:51:48 -0400

    Phases figure work

--- test_files/phases_figure.py
+++ test_files/phases_figure.py
@@ -6,24 +6,34 @@ if __name__ == '__main__':
     # fig, (burst_ax, accretion_ax, cooling_ax) = plt.subplots(1,3,figsize=(10,8))
     fig, burst_ax = plt.subplots(1,1)
 
-    df = pd.read_csv('../flux_saves/Nakazato_2013_s0_AdiabaticMSW_IMO_BstChnl_flux_save.csv')
+    df = pd.read_csv('../flux_saves/Nakazato_2013_s0_NoTransformation_BstChnl_flux_save.csv')
     # need to correct time axis
     time_raw = np.array(df['time'])
-    time = time_raw + time_raw[0]+0.00001
+    offset = 0.00001
+    time = np.abs(time_raw[0]) + time_raw+offset
     print(f'Time offset is {time_raw[0]}')
 
     #TODO: needed to divide by 6 for nux
-    burst_ax.scatter(time, np.array(df['raw_data_nux'])/6,label=r'$\nu_x$')
+    burst_ax.scatter(time, np.array(df['raw_data_nux'])/4,label=r'$\nu_x$') # needs to be divide by four here since we're using truth flux
     burst_ax.scatter(time, df['raw_data_anue'], label=r'$\bar{\nu}_e$')
     burst_ax.scatter(time, df['raw_data_nue'], label=r'$\nu_e$')
-    # burst_ax.set_xscale('log')
     burst_ax.set_ylabel(r'$\frac{neutrinos}{cm^2}$')
-    # burst_ax.set_title('Neutronization Burst')
-    burst_ax.set_title('Cooling')
     burst_ax.legend()
     burst_ax.set_xlabel(r'Time $+ t_0$')
-    burst_ax.set_xlim(16,20)
+
+    # for neutronization burst, log bins
+    # burst_ax.set_title('Neutronization Burst')
+    # burst_ax.set_xscale('log')
+    # burst_ax.set_xlim(0.02, 0.35)
+
+    # for accretion, linear bins
+    # burst_ax.set_title('Accretion')
+    # burst_ax.set_xlim(0.07, 2)
+
+    # for cooling, linear bins
+    burst_ax.set_xlim(14,20)
     burst_ax.set_ylim(0,0.5e9)
+    burst_ax.set_title('Cooling')
 
     # accretion_ax.scatter(time, np.array(df['raw_data_nux'])/6, label=r'$\nu_x$')
     # accretion_ax.scatter(time, df['raw_data_anue'], label=r'$\bar{\nu}_e$')
